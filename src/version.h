a3ada546b2738d2e87e41f1b39c32a72ea2f69fe
ctx-coin <github@centauricoin.info>
2018-07-22 21:26:31 +0200

    End Support for old CTX Cients

--- src/version.h
+++ src/version.h
@@ -15,7 +15,7 @@ static const int PROTOCOL_VERSION = 80015;
 static const int INIT_PROTO_VERSION = 209;
 
 //! In this version, 'getheaders' was introduced.
-static const int GETHEADERS_VERSION = 80004; // CTX: MIN_PEER_PROTO_VERSION got changed in ctx to 80004;
+static const int GETHEADERS_VERSION = 80015; // CTX: MIN_PEER_PROTO_VERSION got changed in ctx to 80004;
 
 //! disconnect from peers older than this proto version
 static const int MIN_PEER_PROTO_VERSION = GETHEADERS_VERSION;
